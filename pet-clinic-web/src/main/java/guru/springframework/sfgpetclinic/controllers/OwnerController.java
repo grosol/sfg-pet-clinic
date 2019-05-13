5dfb915de0fce19d5a3f34c5a959601b8cfd7ff9
grosol <rosgrynyk@gmail.com>
2019-05-13 09:48:47 +0300

    added owners list

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,22 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
+@RequiredArgsConstructor
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
     @RequestMapping({"", "/","/index.html","/index"})
-    public String listOfOwners() {
+    public String listOfOwners(Model model) {
+
+        model.addAttribute("owners",ownerService.findAll());
 
         return "owners/index";
     }
