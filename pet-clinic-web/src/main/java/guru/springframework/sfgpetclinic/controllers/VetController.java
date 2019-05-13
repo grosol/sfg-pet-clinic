ebfd7916f4dc3ad6f4d11628ddbf6145a1ee5e5d
grosol <rosgrynyk@gmail.com>
2019-05-13 10:03:50 +0300

    added vets list

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.VetService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class VetController {
 
+    private final VetService vetService;
+
     @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
-    public String lisOfVets() {
+    public String lisOfVets(Model model)
+    {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
