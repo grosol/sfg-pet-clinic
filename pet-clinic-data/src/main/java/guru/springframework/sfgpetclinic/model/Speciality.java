8a988492634f219981f0875da492b550fb0155a4
rostyslav.grynyk <rostyslav.grynyk@vitech.com.ua>
2019-06-03 10:29:58 +0300

    added Speciality

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Speciality extends BaseEntity {
+
+    private String description;
+}
