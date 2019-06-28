52116a57ea6894703280c8f50f4d7875abf36919
rostyslav.grynyk <rostyslav.grynyk@vitech.com.ua>
2019-06-28 16:14:49 +0300

    close #39

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -2,7 +2,6 @@ package guru.springframework.sfgpetclinic.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
