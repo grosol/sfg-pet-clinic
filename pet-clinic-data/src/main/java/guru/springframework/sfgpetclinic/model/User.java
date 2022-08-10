8cf5673451c651525e0c4a86204b23689a09c0c0
rgrynyk <rostyslav.grynyk@intellias.com>
2022-08-10 19:12:30 +0300

    123456: update User.class

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/User.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/User.java
@@ -3,4 +3,15 @@ package guru.springframework.sfgpetclinic.model;
 public class User {
     private String name;
     private Integer age;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public User(String name, Integer age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
 }
